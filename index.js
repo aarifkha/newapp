297297d07c64226aa7a5fe38d7e126f1fcbb2fbd
Aarif mohammad <haidarali4408@gmail.com>
2022-11-09 23:53:28 +0530

    add all

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express"
 import connectdb from "./config/db.js";
 import router from "./routes/user.route.js";
-
+import { config } from "dotenv";
 
 const app = express();
 app.use(express.json());
 connectdb();
+config()
+
 app.use(router)
-app.listen(2021,(req,res)=>{
+app.listen(process.env.PORT || 2021,(req,res)=>{
     console.log("server connection");
 })
