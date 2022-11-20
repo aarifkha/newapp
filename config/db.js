98ba96f56f24d87917ff1d5e406f44f8012a835f
Aarif mohammad <haidarali4408@gmail.com>
2022-11-20 11:47:39 +0530

    all

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  console.log("MONGO CONNECT++++++++","mongodb+srv://new:qkMUKMeQuq2l3GFL@cluster0.dftrmtm.mongodb.net/test");
     const conn =await mongoose.connect(
 "mongodb+srv://new:qkMUKMeQuq2l3GFL@cluster0.dftrmtm.mongodb.net/test" ,       {
             useNewUrlParser: true,
